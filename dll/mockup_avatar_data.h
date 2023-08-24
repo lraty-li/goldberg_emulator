ed7765a541e3881e323060490d029cee32749563
lraty <lraty@outlook.com>
2023-08-24 23:09:26 +0800

    hardcoded avatars, wait for steam64 id

---
+++ dll/mockup_avatar_data.h
@@ -0,0 +1,22 @@
+#ifndef __INCLUDED_MOCKUP_AVATAR_
+#define __INCLUDED_MOCKUP_AVATAR_
+#include "base.h"
+extern uint8 jm184[184*184*4];
+extern uint8 share184[184*184*4];
+extern uint8 lraty184[184*184*4];
+extern uint8 han184[184*184*4];
+extern uint8 ggbond184[184*184*4];
+
+extern uint8 jm64[64*64*4];
+extern uint8 share64[64*64*4];
+extern uint8 lraty64[64*64*4];
+extern uint8 han64[64*64*4];
+extern uint8 ggbond64[64*64*4];
+
+extern uint8 jm32[32*32*4];
+extern uint8 share32[32*32*4];
+extern uint8 lraty32[32*32*4];
+extern uint8 han32[32*32*4];
+extern uint8 ggbond32[32*32*4];
+
+#endif
